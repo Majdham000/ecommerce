d8e4b95a3d45d4d1545a0f0b55845fbd73efc3c6
Majd HAM <majdhamm00@gmail.com>
2023-05-28 14:37:22 +0300

    Updated the routes

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ResetCodePasswordController;
 
 /*Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
@@ -17,3 +17,7 @@ Route::controller(AuthController::class)->group(function(){
 
 Route::get('products', [ProductController::class, 'index']);
 Route::get('products/{product}', [ProductController::class, 'show']);
+
+Route::post('users/password/email', [ResetCodePasswordController::class , 'userForgetPassword']);
+Route::post('users/password/code/check', [ResetCodePasswordController::class , 'userCheckCode']);
+Route::post('users/password/reset', [ResetCodePasswordController::class , 'userResetPassword']);
